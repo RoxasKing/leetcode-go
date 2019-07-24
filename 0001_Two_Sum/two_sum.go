652fcb338bed93c328e6db7cbdc87fb4f65f5c12
RoxasKing <tekkenfansking@gmail.com>
2019-07-24 09:25:44 +0800

    updated

--- 0001_Two_Sum/two_sum.go
+++ 0001_Two_Sum/two_sum.go
@@ -1,4 +1,6 @@
-package _001_Two_Sum
+package main
+
+import "fmt"
 
 func twoSum(nums []int, target int) []int {
 	// index 负责保存map[整数]整数的序列号
@@ -19,4 +21,10 @@ func twoSum(nums []int, target int) []int {
 	}
 
 	return nil
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	target := 7
+	fmt.Println(twoSum(nums, target))
 }
