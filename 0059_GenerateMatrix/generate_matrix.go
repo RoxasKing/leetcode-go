6c41910bcfedb10aa012f97bd41d2c29762af1ba
RoxasKing <tekkenfansking@gmail.com>
2019-08-09 00:46:03 +0800

    added 0059

--- 0059_GenerateMatrix/generate_matrix.go
+++ 0059_GenerateMatrix/generate_matrix.go
@@ -8,9 +8,49 @@ func generateMatrix(n int) [][]int {
 		row := make([]int, n)
 		out = append(out, row)
 	}
+	num := 0
+	left, right, top, bot := 0, n-1, 0, n-1
+	for {
+		// 首行顺序
+		for j := left; j <= right; j++ {
+			num++
+			out[top][j] = num
+		}
+		if num == n*n {
+			break
+		}
+		top++
+		// 尾列逆序
+		for i := top; i <= bot; i++ {
+			num++
+			out[i][right] = num
+		}
+		if num == n*n {
+			break
+		}
+		right--
+		// 尾行逆序
+		for j := right; j >= left; j-- {
+			num++
+			out[bot][j] = num
+		}
+		if num == n*n {
+			break
+		}
+		bot--
+		// 首列逆序
+		for i := bot; i >= top; i-- {
+			num++
+			out[i][left] = num
+		}
+		if num == n*n {
+			break
+		}
+		left++
+	}
 	return out
 }
 
 func main() {
-	fmt.Println(generateMatrix(3))
+	fmt.Println(generateMatrix(0))
 }
