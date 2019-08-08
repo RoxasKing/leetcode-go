27119bc578499b23ba0450d42baa0a8eb98cf8df
RoxasKing <tekkenfansking@gmail.com>
2019-08-08 16:33:37 +0800

    updated

--- 0051_SolveNQueens/solve_n_queens.go
+++ 0051_SolveNQueens/solve_n_queens.go
@@ -6,14 +6,14 @@ var out [][]string
 
 func solveNQueens(n int) [][]string {
 	out = [][]string{}
-	save := make(map[int]bool)
-	elem := [][]byte{}
-	queen(elem, n, 0, save)
+	rec := make(map[int]bool)
+	tmp := [][]byte{}
+	queen(tmp, n, 0, rec)
 	return out
 }
 
-func queen(elem [][]byte, n, i int, save map[int]bool) {
-	if i >= n {
+func queen(elem [][]byte, n, k int, save map[int]bool) {
+	if k >= n {
 		var res []string
 		for _, v := range elem {
 			res = append(res, string(v))
@@ -22,41 +22,30 @@ func queen(elem [][]byte, n, i int, save map[int]bool) {
 	}
 	isValid := func(elem [][]byte, i, j int) bool {
 		n := 1
-		// 判断对角线上的元素是否有 ‘Q'
-		for k := i - 1; k >= 0; k-- {
-			if j-n >= 0 && elem[k][j-n] == 'Q' || j+n < len(elem[0]) && elem[k][j+n] == 'Q' {
+		for x := i - 1; x >= 0; x-- {
+			if j-n >= 0 && elem[x][j-n] == 'Q' || j+n < len(elem[0]) && elem[x][j+n] == 'Q' {
 				return false
 			}
 			n++
 		}
 		return true
 	}
-	for j := 0; j < n; j++ {
-		if save[j] || !isValid(elem, i, j) {
+	for i := 0; i < n; i++ {
+		if save[i] || !isValid(elem, k, i) {
 			continue
 		}
 		row := []byte{}
-		for k := 0; k < n; k++ {
+		for j := 0; j < n; j++ {
 			row = append(row, '.')
 		}
-		row[j] = 'Q'
-		save[j] = true
-		queen(append(elem, row), n, i+1, save)
-		save[j] = false
+		row[i] = 'Q'
+		save[i] = true
+		queen(append(elem, row), n, k+1, save)
+		save[i] = false
 	}
-}
-
-func isValid(elem [][]byte, i, j int) bool {
-	n := 1
-	for x := i - 1; x >= 0; x-- {
-		if j-n >= 0 && elem[x][j-n] == 'Q' || j+n < len(elem[0]) && elem[x][j+n] == 'Q' {
-			return false
-		}
-		n++
-	}
-	return true
 }
 
 func main() {
-	fmt.Println(solveNQueens(8))
+	anss := solveNQueens(4)
+	fmt.Println(anss)
 }
---
+++ 0052_TotalNQueens/total_n_queens.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var count int
+
+// TODO
+func totalNQueens(n int) int {
+	count = 0
+	if n < 1 {
+		return count
+	}
+	DFS(n, 0, 0, 0, 0)
+	return count
+}
+
+func DFS(n, row, col, pie, na int) {
+	if row >= n {
+		count += 1
+		return
+	}
+	bits := (^(col | pie | na)) & ((1 << uint(n)) - 1)
+	for bits > 0 {
+		p := bits & -bits
+		DFS(n, row+1, col|p, (pie|p)<<1, (na|p)>>1)
+		bits &= bits - 1
+	}
+}
+
+func main() {
+	fmt.Println(totalNQueens(4))
+}
