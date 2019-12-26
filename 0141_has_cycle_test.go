26979113debbed098f4f794f5d8559c269d2a1e3
RoxasKing <tekkenfansking@gmail.com>
2019-12-26 15:40:11 +0800

    added 0141_has_cycle

---
+++ 0141_has_cycle_test.go
@@ -0,0 +1,30 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_hasCycle(t *testing.T) {
+	head := &ListNode{1, nil}
+	head.Next = &ListNode{2, nil}
+	head.Next.Next = &ListNode{3, head}
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test 1",
+			args{head},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasCycle(tt.args.head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
