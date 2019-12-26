854a60cdb4b117032189065a25b6fc6ef7638c54
RoxasKing <tekkenfansking@gmail.com>
2019-12-26 11:36:44 +0800

    updated

--- 0003_length_of_longest_substring.go
+++ 0003_length_of_longest_substring.go
@@ -6,6 +6,25 @@ func lengthOfLongestSubstring(s string) int {
 	for i, j := 0, 0; i < len(s) && j < len(s); j++ {
 		i = max(i, dict[s[j]])
 		out = max(out, j-i+1)
+		if len(s)-i < out {
+			break
+		}
+		dict[s[j]] = j + 1
+	}
+	return out
+}
+
+func longestSubstring(s string) string {
+	var out string
+	dict := make([]int, 128)
+	for i, j := 0, 0; i < len(s) && j < len(s); j++ {
+		i = max(i, dict[s[j]])
+		if len(out) < j-i+1 {
+			out = s[i : j+1]
+		}
+		if len(s)-i < len(out) {
+			break
+		}
 		dict[s[j]] = j + 1
 	}
 	return out
--- 0003_length_of_longest_substring_test.go
+++ 0003_length_of_longest_substring_test.go
@@ -1,6 +1,8 @@
 package My_LeetCode_In_Go
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_lengthOfLongestSubstring(t *testing.T) {
 	type args struct {
@@ -19,6 +21,28 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
 				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_longestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"test 1", args{"abcabcbb"}, "abc"},
+		{"test 2", args{"bbbbbbbb"}, "b"},
+		{"test 3", args{"pwwkew"}, "wke"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("longestSubstring() = %v, want %v", got, tt.want)
 			}
 		})
 	}
