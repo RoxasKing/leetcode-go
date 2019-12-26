fb201b629bb59d782477a0b0c92e001c3917f675
RoxasKing <tekkenfansking@gmail.com>
2019-12-26 15:11:02 +0800

    added 0136_single_number

---
+++ 0136_single_number_test.go
@@ -0,0 +1,25 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test 1", args{[]int{4, 1, 2, 1, 2}}, 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
