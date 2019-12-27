ca74bdd8c224a7e773a2a8ede98830dea8f91b6a
RoxasKing <tekkenfansking@gmail.com>
2019-12-27 17:30:24 +0800

    added 	0005_find_median_sorted_arrays

---
+++ 0004_find_median_sorted_arrays.go
@@ -0,0 +1,7 @@
+package My_LeetCode_In_Go
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	mid := (len(nums1) + len(nums2)) / 2
+	var i,j int
+	// for i<len(nums1)&&j<len(nums2)&&
+}
