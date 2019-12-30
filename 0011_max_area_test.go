3b0546f8d86b27ca40ea3a5166b9b25b651fa7f1
RoxasKing <tekkenfansking@gmail.com>
2019-12-30 10:16:47 +0800

    added 0011_max_area

---
+++ 0011_max_area_test.go
@@ -0,0 +1,23 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_maxArea(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test 1", args{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}}, 49},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxArea(tt.args.height); got != tt.want {
+				t.Errorf("maxArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
