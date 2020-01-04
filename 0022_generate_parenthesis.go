d1d6c1845dd82670c35aaa1cd49514fb30c91c56
RoxasKing <tekkenfansking@gmail.com>
2020-01-04 11:28:17 +0800

    added 0022_generate_parenthesis

---
+++ 0022_generate_parenthesis.go
@@ -0,0 +1,35 @@
+package My_LeetCode_In_Go
+
+/*
+  给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+  例如，给出 n = 3，生成结果为：
+    [
+      "((()))",
+      "(()())",
+      "(())()",
+      "()(())",
+      "()()()"
+    ]
+*/
+
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return nil
+	}
+	var out []string
+	dfs("", n, n, &out)
+	return out
+}
+
+func dfs(str string, l, r int, out *[]string) {
+	if l == 0 && r == 0 {
+		*out = append(*out, str)
+		return
+	}
+	if l > 0 {
+		dfs(str+"(", l-1, r, out)
+	}
+	if r > 0 && r > l {
+		dfs(str+")", l, r-1, out)
+	}
+}
