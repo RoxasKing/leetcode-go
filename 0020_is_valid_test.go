7620ed4fcc4552fba87a1fc6a10e8a11c6b02adf
RoxasKing <tekkenfansking@gmail.com>
2020-01-04 11:07:44 +0800

    added 0020_is_valid

---
+++ 0020_is_valid_test.go
@@ -0,0 +1,28 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"test 1", args{"()"}, true},
+		{"test 2", args{"()[]{}"}, true},
+		{"test 3", args{"(]"}, false},
+		{"test 4", args{"([)]"}, false},
+		{"test 5", args{"{[]}"}, true},
+		{"test 6", args{"}"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
