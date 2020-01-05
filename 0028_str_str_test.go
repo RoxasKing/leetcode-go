e742f64735c9975d19c2d161b20ea632caca9e7b
RoxasKing <tekkenfansking@gmail.com>
2020-01-05 12:57:30 +0800

    added 0028_str_str

---
+++ 0028_str_str_test.go
@@ -0,0 +1,27 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{"", ""}, 0},
+		{"", args{"a", "a"}, 0},
+		{"", args{"hello", "ll"}, 2},
+		{"", args{"aaaaa", "bba"}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
