79bcdb447cc2b3bc959dd87a7c8b78a902bf18bf
RoxasKing <tekkenfansking@gmail.com>
2020-01-11 14:58:13 +0800

    added 0043_multiply

---
+++ 0043_multiply_test.go
@@ -0,0 +1,24 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_multiply(t *testing.T) {
+	type args struct {
+		num1 string
+		num2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"", args{"123", "456"}, "56088"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := multiply(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("multiply() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
