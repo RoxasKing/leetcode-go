2a027629264cf3897f2ea60e00576387524288f9
RoxasKing <tekkenfansking@gmail.com>
2020-01-13 10:41:53 +0800

    added 0045_jump

---
+++ 0045_jump.go
@@ -0,0 +1,44 @@
+package My_LeetCode_In_Go
+
+/*
+  给定一个非负整数数组，你最初位于数组的第一个位置。
+  数组中的每个元素代表你在该位置可以跳跃的最大长度。
+  你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+*/
+
+func jump(nums []int) int {
+	if len(nums) < 2 {
+		return 0
+	}
+	var count, max, next int
+	for i := 0; i < len(nums); {
+		if i+nums[i] >= len(nums)-1 {
+			return count + 1
+		}
+		for j := 1; j <= nums[i]; j++ {
+			if j+nums[i+j] >= max {
+				max = j + nums[i+j]
+				next = j
+			}
+		}
+		i, count, max = i+next, count+1, 0
+	}
+	return count
+}
+
+func jump_2(nums []int) int {
+	var totalJumps, curRange, maxSteps int
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i]+i > maxSteps {
+			maxSteps = nums[i] + i
+		}
+		if i == curRange {
+			totalJumps++
+			curRange = maxSteps
+			if curRange >= len(nums)-1 {
+				break
+			}
+		}
+	}
+	return totalJumps
+}
---
+++ 0045_jump_test.go
@@ -0,0 +1,45 @@
+package My_LeetCode_In_Go
+
+import (
+	"testing"
+)
+
+func Test_jump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{2, 3, 1, 1, 4}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jump(tt.args.nums); got != tt.want {
+				t.Errorf("jump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_jump_2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{2, 3, 1, 1, 4}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jump_2(tt.args.nums); got != tt.want {
+				t.Errorf("jump_2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
