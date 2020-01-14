4ce3e929b2083db2719e2f09c7d7f7600123d0bf
RoxasKing <tekkenfansking@gmail.com>
2020-01-14 17:12:32 +0800

    added 0051_solve_n_queens

---
+++ 0051_solve_n_queens.go
@@ -0,0 +1,57 @@
+package My_LeetCode_In_Go
+
+import "strings"
+
+/*
+  n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+  给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+  每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+*/
+
+func solveNQueens(n int) [][]string {
+	var (
+		out       [][]string
+		track     = make([]string, n)
+		rows      = make([]bool, n)
+		cols      = make([]bool, n)
+		hills     = make([]bool, 2*n-1)
+		dales     = make([]bool, 2*n-1)
+		backtrack func(int)
+	)
+	for i := range track {
+		track[i] = strings.Repeat(".", n)
+	}
+	backtrack = func(row int) {
+		if row == n {
+			out = append(out, append(make([]string, 0, n), track...))
+			return
+		}
+		for col := 0; col < n; col++ {
+			if rows[row] || cols[col] || hills[row-col+n-1] || dales[row+col] {
+				continue
+			}
+
+			tmp := []byte(track[row])
+			tmp[col] = 'Q'
+			track[row] = string(tmp)
+
+			rows[row] = true
+			cols[col] = true
+			dales[row+col] = true
+			hills[row-col+n-1] = true
+
+			backtrack(row + 1)
+
+			rows[row] = false
+			cols[col] = false
+			dales[row+col] = false
+			hills[row-col+n-1] = false
+
+			tmp = []byte(track[row])
+			tmp[col] = '.'
+			track[row] = string(tmp)
+		}
+	}
+	backtrack(0)
+	return out
+}
