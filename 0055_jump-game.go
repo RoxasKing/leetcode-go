e8756080d3539e74f086d33e5d6ae5c8e98984bb
RoxasKing <tekkenfansking@gmail.com>
2020-01-16 13:48:38 +0800

    added 0055_jump-game

---
+++ 0055_jump-game.go
@@ -0,0 +1,17 @@
+package My_LeetCode_In_Go
+
+/*
+  给定一个非负整数数组，你最初位于数组的第一个位置。
+  数组中的每个元素代表你在该位置可以跳跃的最大长度。
+  判断你是否能够到达最后一个位置。
+*/
+
+func canJump(nums []int) bool {
+	lastPos := len(nums) - 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i+nums[i] >= lastPos {
+			lastPos = i
+		}
+	}
+	return lastPos == 0
+}
