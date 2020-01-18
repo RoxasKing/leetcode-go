7bd483f113adac1c2aeb35fc13c18b6afef8b8bd
RoxasKing <tekkenfansking@gmail.com>
2020-01-18 20:58:17 +0800

    added 0060_permutation-sequence

---
+++ 0060_permutation-sequence_test.go
@@ -0,0 +1,25 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_getPermutation(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"", args{4, 9}, "2314"},
+		{"", args{3, 3}, "213"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPermutation(tt.args.n, tt.args.k); got != tt.want {
+				t.Errorf("getPermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
