738413d8482c2f78c6728455c128a0256f0231b1
RoxasKing <tekkenfansking@gmail.com>
2020-01-18 19:26:37 +0800

    updated

---
+++ 0058_length-of-last-word_test.go
@@ -0,0 +1,24 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{""}, 0},
+		{"", args{"Hello World"}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
