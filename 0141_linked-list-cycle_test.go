2c11c0c6ea29b22233a0c4e9223ea06dbdabb902
RoxasKing <tekkenfansking@gmail.com>
2020-01-25 16:51:06 +0800

    updated

--- 0141_linked-list-cycle_test.go
+++ 0141_linked-list-cycle_test.go
@@ -30,30 +30,3 @@ func Test_hasCycle(t *testing.T) {
 		})
 	}
 }
-
-func Test_hasCycle2(t *testing.T) {
-	node1 := &ListNode{1, nil}
-	node1.Next = &ListNode{2, nil}
-	node1.Next.Next = &ListNode{3, node1}
-	node2 := &ListNode{1, nil}
-	node2.Next = &ListNode{2, nil}
-	node2.Next.Next = &ListNode{3, nil}
-	type args struct {
-		head *ListNode
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{"", args{node1}, true},
-		{"", args{node2}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := hasCycle2(tt.args.head); got != tt.want {
-				t.Errorf("hasCycle2() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
