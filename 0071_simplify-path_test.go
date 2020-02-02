b7edc438251de5a2157d39e618bde43482578b6c
RoxasKing <tekkenfansking@gmail.com>
2020-02-02 16:07:06 +0800

    updated

---
+++ 0071_simplify-path_test.go
@@ -0,0 +1,31 @@
+package My_LeetCode_In_Go
+
+import "testing"
+
+func Test_simplifyPath(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"", args{"/a/./b///../c/../././../d/..//../e/./f/./g/././//.//h///././/..///"}, "/e/f/g"},
+		{"", args{"/.."}, "/"},
+		{"", args{"/."}, "/"},
+		{"", args{"/home/"}, "/home"},
+		{"", args{"/../"}, "/"},
+		{"", args{"/home//foo/"}, "/home/foo"},
+		{"", args{"/a/./b/../../c/"}, "/c"},
+		{"", args{"/a/../../b/../c//.//"}, "/c"},
+		{"", args{"/a//b////c/d//././/.."}, "/a/b/c"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := simplifyPath(tt.args.path); got != tt.want {
+				t.Errorf("simplifyPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
