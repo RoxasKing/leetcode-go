c184c53353ff1f5a545386a2d0f9f8fb728c6c66
RoxasKing <tekkenfansking@gmail.com>
2020-02-11 16:39:05 +0800

    added 0084

---
+++ 0084_largest-rectangle-in-histogram.go
@@ -0,0 +1,31 @@
+package leetcode
+
+/*
+  给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+  求在该柱状图中，能够勾勒出来的矩形的最大面积。
+*/
+
+func largestRectangleArea(heights []int) int {
+	stack := make([]int, 0, len(heights)+1)
+	stack = append(stack, -1)
+	var maxArea int
+	for i := range heights {
+		for stack[len(stack)-1] != -1 &&
+			heights[stack[len(stack)-1]] >= heights[i] {
+			maxArea = Max(
+				maxArea,
+				heights[stack[len(stack)-1]]*(i-1-stack[len(stack)-2]),
+			)
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	for stack[len(stack)-1] != -1 {
+		maxArea = Max(
+			maxArea,
+			heights[stack[len(stack)-1]]*(len(heights)-1-stack[len(stack)-2]),
+		)
+		stack = stack[:len(stack)-1]
+	}
+	return maxArea
+}
---
+++ 0084_largest-rectangle-in-histogram_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "testing"
+
+func Test_largestRectangleArea(t *testing.T) {
+	type args struct {
+		heights []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6, 2, 3}}, 15},
+		{"", args{[]int{2, 1, 5, 6, 2, 3}}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestRectangleArea(tt.args.heights); got != tt.want {
+				t.Errorf("largestRectangleArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
