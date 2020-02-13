7e45f5c3bee1c95058ddb088af277d8d47648143
RoxasKing <tekkenfansking@gmail.com>
2020-02-13 18:19:05 +0800

    added 0085

---
+++ 0085_maximal-rectangle.go
@@ -0,0 +1,24 @@
+package leetcode
+
+/*
+  给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+*/
+
+func maximalRectangle(matrix [][]byte) int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return 0
+	}
+	var maxArea int
+	dp := make([]int, len(matrix[0]))
+	for i := range matrix {
+		for j := range matrix[0] {
+			if matrix[i][j] == '1' {
+				dp[j] = dp[j] + 1
+			} else {
+				dp[j] = 0
+			}
+		}
+		maxArea = Max(maxArea, largestRectangleArea(dp))
+	}
+	return maxArea
+}
