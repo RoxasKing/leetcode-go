b3260ab50d40b959472c4f0fd6a3818af9408279
RoxasKing <tekkenfansking@gmail.com>
2020-02-13 21:16:41 +0800

    added 0088

---
+++ 0087_scramble-string_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_isScramble(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"", args{"rgeat", "great"}, true},
+		{"", args{"abcde", "caebd"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isScramble(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("isScramble() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
