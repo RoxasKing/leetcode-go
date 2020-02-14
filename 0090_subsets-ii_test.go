501866ed21dfb6e7ccddb1ca90990a27d28eb4d6
RoxasKing <tekkenfansking@gmail.com>
2020-02-14 16:49:17 +0800

    added 0090

---
+++ 0090_subsets-ii_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_subsetsWithDup(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"",
+			args{[]int{1, 2, 2}},
+			[][]int{{}, {1}, {2}, {1, 2}, {2, 2}, {1, 2, 2}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subsetsWithDup(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subsetsWithDup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
