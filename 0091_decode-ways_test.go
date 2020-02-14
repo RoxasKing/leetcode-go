179a92d24db483cb09eae8b58eeb04e938954815
RoxasKing <tekkenfansking@gmail.com>
2020-02-14 18:01:24 +0800

    added 0091

---
+++ 0091_decode-ways_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_numDecodings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{"01"}, 0},
+		{"", args{"12"}, 2},
+		{"", args{"226"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numDecodings(tt.args.s); got != tt.want {
+				t.Errorf("numDecodings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
