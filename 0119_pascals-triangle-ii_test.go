55b458c4a9e2944dcb01fdb9c42a8e810b9248f7
RoxasKing <tekkenfansking@gmail.com>
2020-02-24 15:25:18 +0800

    added 0119

---
+++ 0119_pascals-triangle-ii_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getRow(t *testing.T) {
+	type args struct {
+		rowIndex int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"", args{3}, []int{1, 3, 3, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getRow(tt.args.rowIndex); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getRow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
