df2d454683555fc07ac5d481e555e499b362cd9c
Roxas King <tekkenfansking@gmail.com>
2020-03-08 10:54:36 +0800

    updated

---
+++ 0322_coin-change_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{1, 2, 5}, 11}, 3},
+		{"", args{[]int{2}, 3}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
