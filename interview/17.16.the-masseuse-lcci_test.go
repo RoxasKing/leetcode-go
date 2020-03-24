953e3ee1b598160e05f6036f9a09a2beb68b7bae
Roxas King <tekkenfansking@gmail.com>
2020-03-24 08:47:57 +0800

    added 17.16

---
+++ interview/17.16.the-masseuse-lcci_test.go
@@ -0,0 +1,26 @@
+package interview
+
+import "testing"
+
+func Test_massage(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{2, 1}}, 2},
+		{"", args{[]int{1, 2, 3, 1}}, 4},
+		{"", args{[]int{2, 7, 9, 3, 1}}, 12},
+		{"", args{[]int{2, 1, 4, 5, 3, 1, 1, 3}}, 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := massage(tt.args.nums); got != tt.want {
+				t.Errorf("massage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ interview/definition.go
@@ -0,0 +1,46 @@
+package interview
+
+// ListNode : Linked list data structure
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// TreeNode : Binary tree data structure
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// Max : Returns the largest number in a and b
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Min : Returns the smallest  number in a and b
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// Abs : Returns the absolute value of n
+func Abs(n int) int {
+	if n <= 0 {
+		return 0 - n
+	}
+	return n
+}
+
+// Gcd : Returns greatest common divisor of a and b
+func Gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
