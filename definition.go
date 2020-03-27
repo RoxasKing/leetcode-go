a7056ede70547cc271b7b62f46e3bf2d7d2f25ee
Roxas King <tekkenfansking@gmail.com>
2020-03-27 09:56:05 +0800

    added 0914

--- definition.go
+++ definition.go
@@ -39,6 +39,11 @@ func Abs(n int) int {
 
 // Gcd : Returns greatest common divisor of a and b
 func Gcd(a, b int) int {
+	// if a == 0 {
+	// 	return b
+	// } else {
+	// 	return Gcd(b%a, a)
+	// }
 	for b != 0 {
 		a, b = b, a%b
 	}
