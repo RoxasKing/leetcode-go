2c2db78e3246a193723d21db41488cb115ee1058
Roxas King <tekkenfansking@gmail.com>
2020-03-31 09:49:10 +0800

    added 0912

---
+++ 0912_sort-an-array_test.go
@@ -0,0 +1,48 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{5, 2, 3, 1}}, []int{1, 2, 3, 5}},
+		{"2", args{[]int{5, 1, 1, 2, 0, 0}}, []int{0, 0, 1, 1, 2, 5}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortArray(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortArray2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{5, 2, 3, 1}}, []int{1, 2, 3, 5}},
+		{"2", args{[]int{5, 1, 1, 2, 0, 0}}, []int{0, 0, 1, 1, 2, 5}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortArray2(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortArray2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
