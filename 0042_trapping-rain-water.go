12d374aba4fcf5de61393f012cf512a2fda18f17
Roxas King <tekkenfansking@gmail.com>
2020-04-04 12:43:37 +0800

    updated 0042

--- 0042_trapping-rain-water.go
+++ 0042_trapping-rain-water.go
@@ -24,3 +24,43 @@ func trap(height []int) int {
 	}
 	return out
 }
+
+// Stack
+func trap2(height []int) int {
+	var out int
+	stack := make([]int, 0, len(height))
+	for i := 0; i < len(height); i++ {
+		for len(stack) != 0 && height[i] > height[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				break
+			}
+			distance := i - stack[len(stack)-1] - 1
+			boundedHeight := Min(height[i], height[stack[len(stack)-1]]) - height[top]
+			out += distance * boundedHeight
+		}
+		stack = append(stack, i)
+	}
+	return out
+}
+
+// Dynamic Programming
+func trap3(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+	lmax, rmax := make([]int, len(height)), make([]int, len(height))
+	lmax[0], rmax[len(height)-1] = height[0], height[len(height)-1]
+	for i := 1; i < len(height); i++ {
+		lmax[i] = Max(lmax[i-1], height[i])
+	}
+	for i := len(height) - 2; i >= 0; i-- {
+		rmax[i] = Max(rmax[i+1], height[i])
+	}
+	var out int
+	for i := range height {
+		out += Min(lmax[i], rmax[i]) - height[i]
+	}
+	return out
+}
--- 0042_trapping-rain-water_test.go
+++ 0042_trapping-rain-water_test.go
@@ -1,6 +1,8 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_trap(t *testing.T) {
 	type args struct {
@@ -17,6 +19,46 @@ func Test_trap(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := trap(tt.args.height); got != tt.want {
 				t.Errorf("trap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trap2(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap2(tt.args.height); got != tt.want {
+				t.Errorf("trap2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trap3(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap3(tt.args.height); got != tt.want {
+				t.Errorf("trap3() = %v, want %v", got, tt.want)
 			}
 		})
 	}
