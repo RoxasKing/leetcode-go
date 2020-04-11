168c5e02bc30710fd8a695007eae2f354ee61a0e
Roxas King <tekkenfansking@gmail.com>
2020-04-11 21:24:32 +0800

    updated

--- 0887_super-egg-drop.go
+++ 0887_super-egg-drop.go
@@ -10,17 +10,19 @@ package leetcode
 */
 
 func superEggDrop(K int, N int) int {
-	dp := make([][]int, K+1)
-	for i := range dp {
-		dp[i] = make([]int, N+1)
-	}
-	for j := 1; j < N; j++ {
-		for i := 1; i <= K; i++ {
-			dp[i][j] = dp[i][j-1] + dp[i-1][j-1] + 1
-			if dp[i][j] >= N {
-				return j
-			}
+	F := 1
+	for {
+		if recurSuperEggDrop(F, K) >= N {
+			break
 		}
+		F++
 	}
-	return N
+	return F
+}
+
+func recurSuperEggDrop(F, K int) int {
+	if F == 1 || K == 1 {
+		return F
+	}
+	return recurSuperEggDrop(F-1, K-1) + 1 + recurSuperEggDrop(F-1, K)
 }
