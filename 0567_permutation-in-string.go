6f2ebb99fb432a406d8e70ec272f64da28ea0511
RoxasKing <tekkenfansking@gmail.com>
2020-04-22 11:54:25 +0800

    added 0567

---
+++ 0567_permutation-in-string.go
@@ -0,0 +1,33 @@
+package leetcode
+
+/*
+  给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+  换句话说，第一个字符串的排列之一是第二个字符串的子串。
+*/
+
+func checkInclusion(s1 string, s2 string) bool {
+	have, need := [26]int{}, [26]int{}
+	for _, c := range s1 {
+		need[c-97]++
+	}
+	var l, r, count int
+	for r < len(s2) {
+		if need[s2[r]-97] == 0 {
+			have = [26]int{}
+			l, r, count = r+1, r+1, 0
+			continue
+		}
+		count++
+		have[s2[r]-97]++
+		for l <= r && have[s2[r]-97] > need[s2[r]-97] {
+			count--
+			have[s2[l]-97]--
+			l++
+		}
+		if count == len(s1) {
+			return true
+		}
+		r++
+	}
+	return false
+}
---
+++ 0567_permutation-in-string_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_checkInclusion(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{"ab", "eidbaooo"}, true},
+		{"2", args{"eidbaooo", "ab"}, false},
+		{"3", args{"ab", "eidboaoo"}, false},
+		{"4", args{"hello", "ooolleoooleh"}, false},
+		{"5", args{"ky", "ainwkckifykxlribaypk"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkInclusion(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("checkInclusion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
