f22aef6174a0abec5ba60a1a1d36e93a7920f24f
RoxasKing <tekkenfansking@gmail.com>
2020-04-23 11:16:46 +0800

    added 08.11

---
+++ interview/08.11.coin-lcci.go
@@ -0,0 +1,29 @@
+package interview
+
+/*
+  硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+*/
+
+func waysToChange(n int) int {
+	mod := 1000000007
+	coins := []int{25, 10, 5, 1}
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for _, coin := range coins {
+		for i := coin; i <= n; i++ {
+			dp[i] = (dp[i] + dp[i-coin]) % mod
+		}
+	}
+	return dp[n]
+}
+
+func waysToChange2(n int) int {
+	var out int
+	mod := 1000000007
+	for i := 0; i*25 <= n; i++ {
+		rest := n - i*25
+		a, b := rest/10, rest%10/5
+		out = (out + (a+1)*(a+b+1)%mod) % mod
+	}
+	return out
+}
---
+++ interview/08.11.coin-lcci_test.go
@@ -0,0 +1,47 @@
+package interview
+
+import (
+	"testing"
+)
+
+func Test_waysToChange(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{5}, 2},
+		{"2", args{10}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := waysToChange(tt.args.n); got != tt.want {
+				t.Errorf("waysToChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_waysToChange2(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{5}, 2},
+		{"2", args{10}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := waysToChange2(tt.args.n); got != tt.want {
+				t.Errorf("waysToChange2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
