5c6606e02c740d13389599dad7171b199b6f1c2b
RoxasKing <tekkenfansking@gmail.com>
2020-04-26 08:55:46 +0800

    updated 0023

--- 0021_merge-two-sorted-lists.go
+++ 0021_merge-two-sorted-lists.go
@@ -4,24 +4,24 @@ package leetcode
   将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 */
 
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	head := &ListNode{Val: -1, Next: nil}
-	node := head
+func mergeTwoLists(l1, l2 *ListNode) *ListNode {
+	head := new(ListNode)
+	tail := head
 	for l1 != nil && l2 != nil {
-		if l1.Val < l2.Val {
-			node.Next = l1
+		if l1.Val <= l2.Val {
+			tail.Next = l1
 			l1 = l1.Next
 		} else {
-			node.Next = l2
+			tail.Next = l2
 			l2 = l2.Next
 		}
-		node = node.Next
+		tail = tail.Next
 	}
 	if l1 != nil {
-		node.Next = l1
+		tail.Next = l1
 	}
 	if l2 != nil {
-		node.Next = l2
+		tail.Next = l2
 	}
 	return head.Next
 }
--- 0023_merge-k-sorted-lists.go
+++ 0023_merge-k-sorted-lists.go
@@ -7,40 +7,31 @@ package leetcode
 func mergeKLists(lists []*ListNode) *ListNode {
 	if len(lists) == 0 {
 		return nil
-	} else if len(lists) == 1 {
-		return lists[0]
 	}
-	base := 1
-	newlists := make([]*ListNode, len(lists))
-	copy(newlists, lists)
-	for base <= len(lists) {
-		for i := 0; i < len(newlists); i += 2 * base {
+	newLists := make([]*ListNode, len(lists))
+	copy(newLists, lists)
+	for base := 1; base <= len(lists); base *= 2 {
+		for i := 0; i < len(newLists); i += 2 * base {
 			if i+base < len(lists) {
-				newlists[i] = mergeTwoLists(newlists[i], newlists[i+base])
+				newLists[i] = mergeTwoLists(newLists[i], newLists[i+base])
 			}
 		}
-		base *= 2
 	}
-	return newlists[0]
+	return newLists[0]
 }
 
+// Recursive
 func mergeKLists2(lists []*ListNode) *ListNode {
 	if len(lists) == 0 {
 		return nil
 	}
-	return mergeK2(&lists, 0, len(lists)-1)
+	return recurMergeKLists(&lists, 0, len(lists)-1)
 }
 
-func mergeK2(lists *[]*ListNode, l, r int) *ListNode {
+func recurMergeKLists(lists *[]*ListNode, l, r int) *ListNode {
 	if l == r {
 		return (*lists)[l]
 	}
-	if l > r {
-		return nil
-	}
-	mid := l + ((r - l) >> 1)
-	return mergeTwoLists(
-		mergeK2(lists, l, mid),
-		mergeK2(lists, mid+1, r),
-	)
+	m := l + (r-l)>>1
+	return mergeTwoLists(recurMergeKLists(lists, l, m), recurMergeKLists(lists, m+1, r))
 }
