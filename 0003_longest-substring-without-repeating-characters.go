9ac6dc91699150717226c7682ef67d6734a1ff81
RoxasKing <tekkenfansking@gmail.com>
2020-05-02 11:08:03 +0800

    updated

--- 0003_longest-substring-without-repeating-characters.go
+++ 0003_longest-substring-without-repeating-characters.go
@@ -4,7 +4,7 @@ func lengthOfLongestSubstring(s string) int {
 	save := make([]int, 128)
 	var max int
 	l, r := 0, 1
-	for max <= len(s)-l && r <= len(s) {
+	for max < len(s)-l && r <= len(s) {
 		l = Max(l, save[s[r-1]])
 		max = Max(max, r-l)
 		save[s[r-1]] = r
