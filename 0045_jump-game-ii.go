e5b83d3fefa64a484e00c4cdf07b72b0ac51721d
RoxasKing <tekkenfansking@gmail.com>
2020-05-04 08:16:29 +0800

    updated 0045

--- 0045_jump-game-ii.go
+++ 0045_jump-game-ii.go
@@ -10,35 +10,36 @@ func jump(nums []int) int {
 	if len(nums) < 2 {
 		return 0
 	}
-	var count, max, next int
-	for i := 0; i < len(nums); {
+	var steps, nextPace int
+	for i := 0; i < len(nums); i += nextPace {
+		steps++
 		if i+nums[i] >= len(nums)-1 {
-			return count + 1
+			break
 		}
+		var max int // Record the maximum distance that can be reached next
 		for j := 1; j <= nums[i]; j++ {
-			if j+nums[i+j] >= max {
+			if j+nums[i+j] > max {
 				max = j + nums[i+j]
-				next = j
+				nextPace = j
 			}
 		}
-		i, count, max = i+next, count+1, 0
 	}
-	return count
+	return steps
 }
 
 func jump2(nums []int) int {
-	var totalJumps, curRange, maxSteps int
+	var steps, cur, max int
 	for i := 0; i < len(nums)-1; i++ {
-		if nums[i]+i > maxSteps {
-			maxSteps = nums[i] + i
+		if nums[i]+i > max {
+			max = nums[i] + i
 		}
-		if i == curRange {
-			totalJumps++
-			curRange = maxSteps
-			if curRange >= len(nums)-1 {
+		if i == cur {
+			steps++
+			cur = max
+			if cur >= len(nums)-1 {
 				break
 			}
 		}
 	}
-	return totalJumps
+	return steps
 }
