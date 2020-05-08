25885958d9bd21cfb84f8344a1934bd7028d5697
RoxasKing <tekkenfansking@gmail.com>
2020-05-08 09:58:12 +0800

    updated

--- 0221_maximal-square.go
+++ 0221_maximal-square.go
@@ -13,7 +13,7 @@ func maximalSquare(matrix [][]byte) int {
 			if matrix[i][j] != '1' {
 				continue
 			}
-			dp[i][j] = int(matrix[i][j] - '0')
+			dp[i][j] = 1
 			if i-1 >= 0 && j-1 >= 0 {
 				dp[i][j] += Min(Min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])
 			}
