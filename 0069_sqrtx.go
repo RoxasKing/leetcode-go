ed91a643aec353cbae93f338ea92c8b5c04b0b06
RoxasKing <tekkenfansking@gmail.com>
2020-05-09 09:07:25 +0800

    updated

--- 0069_sqrtx.go
+++ 0069_sqrtx.go
@@ -6,17 +6,18 @@ package leetcode
   由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
 */
 
+// Dichotomy
 func mySqrt(x int) int {
 	l, r := 0, x
 	for l <= r {
-		mid := l + (r-l)>>1
-		switch {
-		case mid*mid < x:
-			l = mid + 1
-		case mid*mid > x:
-			r = mid - 1
-		case mid*mid == x:
-			return mid
+		m := l + (r-l)>>1
+		tmp := m * m
+		if tmp < x {
+			l = m + 1
+		} else if tmp > x {
+			r = m - 1
+		} else {
+			return m
 		}
 	}
 	return l - 1
