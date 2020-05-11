d3dd0c6242f4edabc900664c24c6741462d2833e
RoxasKing <tekkenfansking@gmail.com>
2020-05-11 20:09:30 +0800

    added coding_interviews 18

---
+++ coding_interviews/18.shan-chu-lian-biao-de-jie-dian-lcof_test.go
@@ -0,0 +1,49 @@
+package codinginterviews
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_deleteNode(t *testing.T) {
+	type args struct {
+		head *ListNode
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"1",
+			args{
+				&ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val:  1,
+							Next: &ListNode{Val: 9},
+						},
+					},
+				},
+				5,
+			},
+			&ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val:  1,
+					Next: &ListNode{Val: 9},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteNode(tt.args.head, tt.args.val); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
