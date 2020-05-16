eeae04e6704fcaf7e26855f02ddea1c82a3c05f8
RoxasKing <tekkenfansking@gmail.com>
2020-05-16 23:10:41 +0800

    added coding_interviews 27

---
+++ coding_interviews/27.er-cha-shu-de-jing-xiang-lcof_test.go
@@ -0,0 +1,56 @@
+package codinginterviews
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mirrorTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			"1",
+			args{
+				&TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 1},
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{
+						Val:   7,
+						Left:  &TreeNode{Val: 6},
+						Right: &TreeNode{Val: 9},
+					},
+				},
+			},
+			&TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val:   7,
+					Left:  &TreeNode{Val: 9},
+					Right: &TreeNode{Val: 6},
+				},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 3},
+					Right: &TreeNode{Val: 1},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mirrorTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mirrorTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
