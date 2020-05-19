5802e80be2d80c73c791cae0de2d338529defb25
RoxasKing <tekkenfansking@gmail.com>
2020-05-19 23:08:07 +0800

    added coding_interviews 32-I

---
+++ coding_interviews/32-I.cong-shang-dao-xia-da-yin-er-cha-shu-lcof.go
@@ -0,0 +1,28 @@
+package codinginterviews
+
+/*
+  从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+  提示：
+    节点总数 <= 1000
+*/
+
+// Preorder traversal + BFS
+func levelOrder(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var out []int
+	q := []*TreeNode{root}
+	for len(q) != 0 {
+		root, q = q[0], q[1:]
+		out = append(out, root.Val)
+		if root.Left != nil {
+			q = append(q, root.Left)
+		}
+		if root.Right != nil {
+			q = append(q, root.Right)
+		}
+	}
+	return out
+}
