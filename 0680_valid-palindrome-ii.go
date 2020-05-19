50beaa182729f7b81cea4b27e77cf8f3d7f37ea3
RoxasKing <tekkenfansking@gmail.com>
2020-05-19 09:42:57 +0800

    added 0680

---
+++ 0680_valid-palindrome-ii.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/*
+  给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+  注意:
+    字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+*/
+
+func validPalindrome(s string) bool {
+	isPalindrome := func(l, r int) bool {
+		for l < r {
+			if s[l] != s[r] {
+				return false
+			}
+			l++
+			r--
+		}
+		return true
+	}
+	l, r := 0, len(s)-1
+	for l < r {
+		if s[l] != s[r] {
+			return isPalindrome(l+1, r) || isPalindrome(l, r-1)
+		}
+		l++
+		r--
+	}
+	return true
+}
---
+++ 0680_valid-palindrome-ii_test.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "testing"
+
+func Test_validPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{"aba"}, true},
+		{"2", args{"abca"}, true},
+		{"3", args{"abcda"}, false},
+		{"4", args{"a"}, true},
+		{"5", args{"aa"}, true},
+		{"6", args{"eeccccbebaeeabebccceea"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("validPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
