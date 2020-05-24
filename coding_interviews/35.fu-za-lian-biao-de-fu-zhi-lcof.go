78491bb029c1b07795a3e5a0643a128f7fcd66fc
RoxasKing <tekkenfansking@gmail.com>
2020-05-24 19:45:51 +0800

    added coding_interviews 35

---
+++ coding_interviews/35.fu-za-lian-biao-de-fu-zhi-lcof.go
@@ -0,0 +1,53 @@
+package codinginterviews
+
+/*
+  请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+
+  提示：
+    -10000 <= Node.val <= 10000
+    Node.random 为空（null）或指向链表中的节点。
+    节点数目不超过 1000 。
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func copyRandomList(head *Node) *Node {
+	cloneNodes(head)
+	radomLinks(head)
+	return splitList(head)
+}
+
+func cloneNodes(head *Node) {
+	for n := head; n != nil; n = n.Next.Next {
+		clone := &Node{Val: n.Val}
+		clone.Next = n.Next
+		n.Next = clone
+	}
+}
+
+func radomLinks(head *Node) {
+	for n := head; n != nil; n = n.Next.Next {
+		if n.Random != nil {
+			n.Next.Random = n.Random.Next
+		}
+	}
+}
+
+func splitList(head *Node) *Node {
+	pre := &Node{}
+	cur := pre
+	for n := head; n != nil; n = n.Next {
+		cur.Next = n.Next
+		cur = n.Next
+		n.Next = n.Next.Next
+	}
+	return pre.Next
+}
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
---
+++ coding_interviews/35.fu-za-lian-biao-de-fu-zhi-lcof_test.go
@@ -0,0 +1,54 @@
+package codinginterviews
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_copyRandomList(t *testing.T) {
+	ex1 := &Node{Val: 7}
+	ex11 := &Node{Val: 13}
+	ex12 := &Node{Val: 11}
+	ex13 := &Node{Val: 10}
+	ex14 := &Node{Val: 1}
+	ex1.Next = ex11
+	ex11.Next = ex12
+	ex11.Random = ex1
+	ex12.Next = ex13
+	ex12.Random = ex14
+	ex13.Next = ex14
+	ex13.Random = ex12
+	ex14.Random = ex1
+
+	ex2 := &Node{Val: 1}
+	ex21 := &Node{Val: 2}
+	ex2.Next = ex21
+	ex21.Random = ex21
+
+	ex3 := &Node{Val: 3}
+	ex31 := &Node{Val: 3}
+	ex32 := &Node{Val: 3}
+	ex3.Next = ex31
+	ex31.Next = ex32
+	ex31.Random = ex3
+
+	type args struct {
+		head *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		{"1", args{ex1}, ex1},
+		{"2", args{ex2}, ex2},
+		{"3", args{ex3}, ex3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := copyRandomList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("copyRandomList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
