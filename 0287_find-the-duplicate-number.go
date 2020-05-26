04395b8f9dcc142b76928262457ed7d1186d7627
RoxasKing <tekkenfansking@gmail.com>
2020-05-26 14:34:21 +0800

    updated

--- 0287_find-the-duplicate-number.go
+++ 0287_find-the-duplicate-number.go
@@ -44,12 +44,12 @@ func findDuplicate2(nums []int) int {
 		bitMax--
 	}
 	for bit := 0; bit <= bitMax; bit++ {
-		x, y := 0, 0
+		var x, y int
 		for i, num := range nums {
-			if num&(1<<bit) > 0 {
+			if num&(1<<bit) != 0 {
 				x++
 			}
-			if i > 0 && i&(1<<bit) > 0 {
+			if i > 0 && i&(1<<bit) != 0 {
 				y++
 			}
 		}
