ba520d9ea920b1b66713757e36a339e18a65ba1d
RoxasKing <tekkenfansking@gmail.com>
2020-05-30 21:15:03 +0800

    updated

---
+++ coding_interviews/43-I.1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof.go
@@ -0,0 +1,20 @@
+package codinginterviews
+
+/*
+  输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+  例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+
+  限制：
+    1 <= n < 2^31
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func countDigitOne(n int) int {
+	var out int
+	for i := 1; i <= n; i++ {
+	}
+	return out
+}
