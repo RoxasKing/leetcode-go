2e563df03390c8cc1bb71ac949431b821392aea0
RoxasKing <tekkenfansking@gmail.com>
2020-05-30 09:52:16 +0800

    updated

--- 0084_largest-rectangle-in-histogram.go
+++ 0084_largest-rectangle-in-histogram.go
@@ -9,21 +9,14 @@ func largestRectangleArea(heights []int) int {
 	stack := append(make([]int, 0, len(heights)+1), -1)
 	var out int
 	for i := range heights {
-		for stack[len(stack)-1] != -1 &&
-			heights[stack[len(stack)-1]] >= heights[i] {
-			out = Max(
-				out,
-				heights[stack[len(stack)-1]]*(i-1-stack[len(stack)-2]),
-			)
+		for stack[len(stack)-1] != -1 && heights[stack[len(stack)-1]] >= heights[i] {
+			out = Max(out, heights[stack[len(stack)-1]]*(i-1-stack[len(stack)-2]))
 			stack = stack[:len(stack)-1]
 		}
 		stack = append(stack, i)
 	}
 	for stack[len(stack)-1] != -1 {
-		out = Max(
-			out,
-			heights[stack[len(stack)-1]]*(len(heights)-1-stack[len(stack)-2]),
-		)
+		out = Max(out, heights[stack[len(stack)-1]]*(len(heights)-1-stack[len(stack)-2]))
 		stack = stack[:len(stack)-1]
 	}
 	return out
