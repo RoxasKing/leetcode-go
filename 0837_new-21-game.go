b0c5d88513d9829f335b815ce81fc3e24b3ddd48
RoxasKing <tekkenfansking@gmail.com>
2020-06-03 17:18:57 +0800

    updated

--- 0837_new-21-game.go
+++ 0837_new-21-game.go
@@ -22,13 +22,13 @@ func new21Game(N int, K int, W int) float64 {
 	if K == 0 {
 		return 1
 	}
-	dp := make([]float64, K+W+1)
+	dp := make([]float64, K+W)
 	for i := K; i <= N && i < K+W; i++ {
 		dp[i] = 1
 	}
 	dp[K-1] = float64(Min(N-K+1, W)) / float64(W)
 	for i := K - 2; i >= 0; i-- {
-		dp[i] = dp[i+1] - (dp[i+W+1]-dp[i+1])/float64(W)
+		dp[i] = dp[i+1] - (dp[i+1+W]-dp[i+1])/float64(W)
 	}
 	return dp[0]
 }
