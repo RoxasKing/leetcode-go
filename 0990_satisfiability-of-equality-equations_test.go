6e31d712aa04daa5174d2ad9a9dc700691cb36ba
RoxasKing <tekkenfansking@gmail.com>
2020-06-08 09:45:49 +0800

    added 0990

---
+++ 0990_satisfiability-of-equality-equations_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "testing"
+
+func Test_equationsPossible(t *testing.T) {
+	type args struct {
+		equations []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{[]string{"a==b", "b!=a"}}, false},
+		{"2", args{[]string{"b==a", "a==b"}}, true},
+		{"3", args{[]string{"a==b", "b==c", "a==c"}}, true},
+		{"4", args{[]string{"a==b", "b!=c", "c==a"}}, false},
+		{"5", args{[]string{"c==c", "b==d", "x!=z"}}, true},
+		{"6", args{[]string{"a!=a"}}, false},
+		{"7", args{[]string{"e==e", "d!=e", "c==d", "d!=e"}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := equationsPossible(tt.args.equations); got != tt.want {
+				t.Errorf("equationsPossible() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
