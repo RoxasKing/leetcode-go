c0cdf8cc9c4c34b0abe018d73c4a04229ee9aedf
RoxasKing <tekkenfansking@gmail.com>
2020-06-12 21:10:36 +0800

    added coding_interviews 57-II

---
+++ coding_interviews/57-II.he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof.go
@@ -0,0 +1,36 @@
+package codinginterviews
+
+/*
+  输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+
+  序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+  限制：
+    1 <= target <= 10^5
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func findContinuousSequence(target int) [][]int {
+	var out [][]int
+	l, r := 1, 2
+	for l < r {
+		sum := (l + r) * (r + 1 - l) >> 1
+		switch {
+		case sum < target:
+			r++
+		case sum > target:
+			l++
+		default:
+			tmp := make([]int, r+1-l)
+			for i := range tmp {
+				tmp[i] = i + l
+			}
+			out = append(out, tmp)
+			l++
+		}
+	}
+	return out
+}
