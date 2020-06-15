7abe3d888c227e7a1d7640b14ed3c136b4ce71df
RoxasKing <tekkenfansking@gmail.com>
2020-06-15 21:13:29 +0800

    added coding_interviews 64

---
+++ coding_interviews/64.qiu-12n-lcof.go
@@ -0,0 +1,72 @@
+package codinginterviews
+
+/*
+  求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+  限制：
+    1 <= n <= 10000
+*/
+
+func sumNums(n int) int {
+	ans, A, B := 0, n, n+1
+	addGreatZero := func() bool {
+		ans += A
+		return ans > 0
+	}
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	B >>= 1
+
+	_ = ((B & 1) > 0) && addGreatZero()
+	A <<= 1
+	return ans >> 1
+}
---
+++ coding_interviews/64.qiu-12n-lcof_test.go
@@ -0,0 +1,24 @@
+package codinginterviews
+
+import "testing"
+
+func Test_sumNums(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{3}, 6},
+		{"2", args{9}, 45},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumNums(tt.args.n); got != tt.want {
+				t.Errorf("sumNums() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
