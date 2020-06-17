e6b66da0a9b20d84c2c3732983dc09a569f091c3
RoxasKing <tekkenfansking@gmail.com>
2020-06-17 09:11:21 +0800

    added 1014

---
+++ 1014_best-sightseeing-pair_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "testing"
+
+func Test_maxScoreSightseeingPair(t *testing.T) {
+	type args struct {
+		A []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{[]int{8, 1, 5, 2, 6}}, 11},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScoreSightseeingPair(tt.args.A); got != tt.want {
+				t.Errorf("maxScoreSightseeingPair() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
