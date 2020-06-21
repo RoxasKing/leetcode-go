c828f7ddaa1955b2888159ef5827e614ea88fe26
RoxasKing <tekkenfansking@gmail.com>
2020-06-21 20:17:18 +0800

    added coding_interviews 68-II

--- coding_interviews/68-I.lowestCommonAncestor.go
+++ coding_interviews/68-I.lowestCommonAncestor.go
@@ -30,3 +30,20 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	}
 	return search(root)
 }
+
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	if p.Val > q.Val {
+		p, q = q, p
+	}
+	var out *TreeNode
+	for out == nil {
+		if root.Val > q.Val {
+			root = root.Left
+		} else if root.Val < p.Val {
+			root = root.Right
+		} else {
+			out = root
+		}
+	}
+	return out
+}
