bfd3d1f321a72e257dd1cd8fb406f9658fcaa39b
RoxasKing <tekkenfansking@gmail.com>
2020-06-24 00:56:02 +0800

    updated

--- 0016_3sum-closest.go
+++ 0016_3sum-closest.go
@@ -5,52 +5,36 @@ import "sort"
 /*
   给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
 */
-
 func threeSumClosest(nums []int, target int) int {
 	sort.Ints(nums)
 	out := nums[0] + nums[1] + nums[2]
-	var i int
-	for i <= len(nums)-3 {
-		if nums[i] >= target/3 {
-			return out
+	for i := 0; i < len(nums)-2; i++ {
+		if nums[i] > target/3 {
+			break
 		}
-		head, tail := i+1, len(nums)-1
-		for head < tail {
-			sum := nums[i] + nums[head] + nums[tail]
-			if sum == target {
-				return sum
-			} else if sum < target {
-				out = closest(sum, out, target)
-				for head < tail && nums[head] == nums[head+1] {
-					head++
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum < target {
+				for l+1 < r && nums[l] == nums[l+1] {
+					l++
 				}
-				head++
+				l++
+			} else if sum > target {
+				for l < r-1 && nums[r-1] == nums[r] {
+					r--
+				}
+				r--
 			} else {
-				out = closest(sum, out, target)
-				for head < tail && nums[tail] == nums[tail-1] {
-					tail--
-				}
-				tail--
+				return sum
+			}
+			if Abs(sum-target) < Abs(out-target) {
+				out = sum
 			}
 		}
-		for i < len(nums)-3 && nums[i] == nums[i+1] {
+		for i+1 < len(nums)-2 && nums[i] == nums[i+1] {
 			i++
 		}
-		i++
 	}
 	return out
-}
-
-func closest(a, b, target int) int {
-	t1, t2 := a-target, b-target
-	if t1 < 0 {
-		t1 = -t1
-	}
-	if t2 < 0 {
-		t2 = -t2
-	}
-	if t1 < t2 {
-		return a
-	}
-	return b
 }
--- 0016_3sum-closest_test.go
+++ 0016_3sum-closest_test.go
@@ -12,7 +12,9 @@ func Test_threeSumClosest(t *testing.T) {
 		args args
 		want int
 	}{
-		{"", args{[]int{-1, 2, 1, -4}, 1}, 2},
+		{"1", args{[]int{-1, 2, 1, -4}, 1}, 2},
+		{"2", args{[]int{1, 1, 1, 0}, -100}, 2},
+		{"3", args{[]int{-4, -7, -2, 2, 5, -2, 1, 9, 3, 9, 4, 9, -9, -3, 7, 4, 1, 0, 8, 5, -7, -7}, 29}, 27},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
