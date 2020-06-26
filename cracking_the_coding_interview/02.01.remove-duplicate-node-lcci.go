4245183672af36293bfef3959f2767371459f6c1
RoxasKing <tekkenfansking@gmail.com>
2020-06-26 10:50:33 +0800

    added cracking_the_coding_interview 02.01

---
+++ cracking_the_coding_interview/02.01.remove-duplicate-node-lcci.go
@@ -0,0 +1,45 @@
+package crackingthecodingintervew
+
+/*
+  编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+
+  提示：
+    链表长度在[0, 20000]范围内。
+    链表元素在[0, 20000]范围内。
+
+  进阶：
+    如果不得使用临时缓冲区，该怎么解决？
+*/
+
+// Hash
+func removeDuplicateNodes(head *ListNode) *ListNode {
+	mark := make(map[int]struct{})
+	pre := &ListNode{Val: -1, Next: head}
+	cur := pre
+	for cur != nil && cur.Next != nil {
+		if _, ok := mark[cur.Next.Val]; !ok {
+			mark[cur.Next.Val] = struct{}{}
+			cur = cur.Next
+		} else {
+			cur.Next = cur.Next.Next
+		}
+	}
+	return pre.Next
+}
+
+// Enumerate + Double Loop
+func removeDuplicateNodes2(head *ListNode) *ListNode {
+	cur := head
+	for cur != nil {
+		ptr := cur
+		for ptr.Next != nil {
+			if ptr.Next.Val == cur.Val {
+				ptr.Next = ptr.Next.Next
+			} else {
+				ptr = ptr.Next
+			}
+		}
+		cur = cur.Next
+	}
+	return head
+}
