7cb57b89eb1800c495dbc57432fb8eccea59c9ca
RoxasKing <tekkenfansking@gmail.com>
2020-06-29 20:34:36 +0800

    added 0167

---
+++ 0167_two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,67 @@
+package leetcode
+
+/*
+  给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+  函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+  说明:
+    返回的下标值（index1 和 index2）不是从零开始的。
+    你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// Double Pointer
+func twoSumII(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum < target {
+			l++
+		} else if sum > target {
+			r--
+		} else {
+			return []int{l + 1, r + 1}
+		}
+	}
+	return nil
+}
+
+// Binary Search
+func twoSumII2(numbers []int, target int) []int {
+	binarySearch := func(l, r, target int) int {
+		for l <= r {
+			m := l + (r-l)>>1
+			if numbers[m] < target {
+				l = m + 1
+			} else if numbers[m] > target {
+				r = m - 1
+			} else {
+				return m
+			}
+		}
+		return -1
+	}
+	for i := 0; i < len(numbers)-1 && numbers[i] <= target/2; i++ {
+		if j := binarySearch(i+1, len(numbers)-1, target-numbers[i]); j != -1 {
+			return []int{i + 1, j + 1}
+		}
+	}
+	return nil
+}
+
+// Hash
+func twoSumII3(numbers []int, target int) []int {
+	mark := make(map[int]int)
+	for i, num := range numbers {
+		mark[num] = i
+	}
+	for i := 0; i < len(numbers) && numbers[i] <= target/2; i++ {
+		if j, ok := mark[target-numbers[i]]; ok {
+			return []int{i + 1, j + 1}
+		}
+	}
+	return nil
+}
---
+++ 0167_two-sum-ii-input-array-is-sorted_test.go
@@ -0,0 +1,69 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSumII(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{2, 7, 11, 15}, 9}, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSumII(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSumII2(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{2, 7, 11, 15}, 9}, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSumII2(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumII2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSumII3(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{2, 7, 11, 15}, 9}, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSumII3(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumII3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
