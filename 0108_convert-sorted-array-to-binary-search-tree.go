49c0d48cf07d44ff39c94a8debb5689e5943f772
RoxasKing <tekkenfansking@gmail.com>
2020-07-03 00:34:11 +0800

    updated 0108

--- 0108_convert-sorted-array-to-binary-search-tree.go
+++ 0108_convert-sorted-array-to-binary-search-tree.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math/rand"
+
 /*
   将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
   本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
@@ -14,20 +16,46 @@ package leetcode
 */
 
 func sortedArrayToBST(nums []int) *TreeNode {
-	if len(nums) == 0 {
-		return nil
-	}
 	var buildBST func(int, int) *TreeNode
-	buildBST = func(left, right int) *TreeNode {
-		if left == right {
+	buildBST = func(l, r int) *TreeNode {
+		if l > r {
 			return nil
 		}
-		mid := left + (right-left)>>1
-		return &TreeNode{
-			Val:   nums[mid],
-			Left:  buildBST(left, mid),
-			Right: buildBST(mid+1, right),
-		}
+		m := l + (r-l)>>1
+		root := &TreeNode{Val: nums[m]}
+		root.Left = buildBST(l, m-1)
+		root.Right = buildBST(m+1, r)
+		return root
 	}
-	return buildBST(0, len(nums))
+	return buildBST(0, len(nums)-1)
+}
+
+func sortedArrayToBST2(nums []int) *TreeNode {
+	var buildBST func(int, int) *TreeNode
+	buildBST = func(l, r int) *TreeNode {
+		if l > r {
+			return nil
+		}
+		m := l + (r+1-l)>>1
+		root := &TreeNode{Val: nums[m]}
+		root.Left = buildBST(l, m-1)
+		root.Right = buildBST(m+1, r)
+		return root
+	}
+	return buildBST(0, len(nums)-1)
+}
+
+func sortedArrayToBST3(nums []int) *TreeNode {
+	var buildBST func(int, int) *TreeNode
+	buildBST = func(l, r int) *TreeNode {
+		if l > r {
+			return nil
+		}
+		m := l + (r+rand.Intn(2)-l)>>1
+		root := &TreeNode{Val: nums[m]}
+		root.Left = buildBST(l, m-1)
+		root.Right = buildBST(m+1, r)
+		return root
+	}
+	return buildBST(0, len(nums)-1)
 }
--- 0108_convert-sorted-array-to-binary-search-tree_test.go
+++ 0108_convert-sorted-array-to-binary-search-tree_test.go
@@ -14,26 +14,52 @@ func Test_sortedArrayToBST(t *testing.T) {
 		args args
 		want *TreeNode
 	}{
-		{
-			"",
-			args{[]int{-10, -3, 0, 5, 9}},
-			&TreeNode{
-				Val: 0,
-				Left: &TreeNode{
-					Val:  -3,
-					Left: &TreeNode{Val: -10},
-				},
-				Right: &TreeNode{
-					Val:  9,
-					Left: &TreeNode{Val: 5},
-				},
-			},
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortedArrayToBST2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST2(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortedArrayToBST3(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST3(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST3() = %v, want %v", got, tt.want)
 			}
 		})
 	}
