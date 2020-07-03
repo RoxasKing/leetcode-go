edd3c35a7c4314c7b3744648c1702442998465f9
RoxasKing <tekkenfansking@gmail.com>
2020-07-04 00:51:29 +0800

    updated

--- 0032_longest-valid-parentheses.go
+++ 0032_longest-valid-parentheses.go
@@ -9,24 +9,21 @@ func longestValidParentheses(s string) int {
 	var max int
 	dp := make([]int, len(s))
 	for i := 1; i < len(s); i++ {
-		if s[i] == ')' {
-			if s[i-1] == '(' {
-				if i >= 2 {
-					dp[i] = dp[i-2] + 2
-				} else {
-					dp[i] = 2
-				}
-			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
-				if i-dp[i-1] >= 2 {
-					dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
-				} else {
-					dp[i] = dp[i-1] + 2
-				}
+		if s[i] == '(' {
+			continue
+		}
+		if s[i-1] == '(' {
+			dp[i] = 2
+			if i-2 >= 0 {
+				dp[i] += dp[i-2]
 			}
-			if dp[i] > max {
-				max = dp[i]
+		} else if i-dp[i-1]-1 >= 0 && s[i-dp[i-1]-1] == '(' {
+			dp[i] = dp[i-1] + 2
+			if i-dp[i-1]-2 >= 0 {
+				dp[i] += dp[i-dp[i-1]-2]
 			}
 		}
+		max = Max(max, dp[i])
 	}
 	return max
 }
