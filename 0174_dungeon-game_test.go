53d403668a96a2a4bf8b402208276e4ba4e9ef8d
RoxasKing <tekkenfansking@gmail.com>
2020-07-12 10:12:49 +0800

    added 0174

---
+++ 0174_dungeon-game_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_calculateMinimumHP(t *testing.T) {
+	type args struct {
+		dungeon [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"1",
+			args{
+				[][]int{
+					{-2, -3, 3},
+					{-5, -10, 1},
+					{10, 30, -5},
+				},
+			},
+			7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateMinimumHP(tt.args.dungeon); got != tt.want {
+				t.Errorf("calculateMinimumHP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
