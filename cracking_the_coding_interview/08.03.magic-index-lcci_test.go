39ed23ce7e0269c8397e632a25598ffb5f134323
RoxasKing <tekkenfansking@gmail.com>
2020-07-31 23:14:18 +0800

    added 08.03

---
+++ cracking_the_coding_interview/08.03.magic-index-lcci_test.go
@@ -0,0 +1,24 @@
+package crackingthecodingintervew
+
+import "testing"
+
+func Test_findMagicIndex(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{[]int{0, 2, 3, 4, 5}}, 0},
+		{"2", args{[]int{1, 1, 1}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMagicIndex(tt.args.nums); got != tt.want {
+				t.Errorf("findMagicIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
