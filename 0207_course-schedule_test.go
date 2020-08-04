69451ccfa2b0f6438bd969605275e380a0198804
RoxasKing <tekkenfansking@gmail.com>
2020-08-04 10:20:13 +0800

    added 0207

---
+++ 0207_course-schedule_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_canFinish(t *testing.T) {
+	type args struct {
+		numCourses    int
+		prerequisites [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{2, [][]int{{1, 0}}}, true},
+		{"2", args{2, [][]int{{1, 0}, {0, 1}}}, false},
+		{"3", args{3, [][]int{{1, 0}, {1, 2}, {0, 1}}}, false},
+		{"4", args{3, [][]int{{0, 2}, {1, 2}, {2, 0}}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canFinish(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+				t.Errorf("canFinish() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_canFinish2(t *testing.T) {
+	type args struct {
+		numCourses    int
+		prerequisites [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{2, [][]int{{1, 0}}}, true},
+		{"2", args{2, [][]int{{1, 0}, {0, 1}}}, false},
+		{"3", args{3, [][]int{{1, 0}, {1, 2}, {0, 1}}}, false},
+		{"4", args{3, [][]int{{0, 2}, {1, 2}, {2, 0}}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canFinish2(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+				t.Errorf("canFinish2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
