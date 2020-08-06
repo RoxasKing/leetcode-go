6ef7a62466cc16fa28655795ff91deec5a44a757
RoxasKing <tekkenfansking@gmail.com>
2020-08-06 08:40:37 +0800

    added 0336

---
+++ 0336_palindrome-pairs_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_palindromePairs(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"1",
+			args{[]string{"abcd", "dcba", "lls", "s", "sssll"}},
+			[][]int{{0, 1}, {1, 0}, {3, 2}, {2, 4}},
+		},
+		{
+			"2",
+			args{[]string{"bat", "tab", "cat"}},
+			[][]int{{0, 1}, {1, 0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := palindromePairs(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("palindromePairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
