473dd49e9370646d3caad36a0af8c85dd25b293a
RoxasKing <tekkenfansking@gmail.com>
2020-08-06 09:55:29 +0800

    updated 0336

--- 0336_palindrome-pairs.go
+++ 0336_palindrome-pairs.go
@@ -61,3 +61,45 @@ func palindromePairs(words []string) [][]int {
 	}
 	return out
 }
+
+// Hash
+func palindromePairs2(words []string) [][]int {
+	revWord := func(word string) string {
+		bytes := []byte(word)
+		for i := 0; i < len(bytes)>>1; i++ {
+			bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i]
+		}
+		return string(bytes)
+	}
+
+	isPalindrome := func(s string) bool {
+		for i := 0; i < len(s)>>1; i++ {
+			if s[i] != s[len(s)-1-i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	dict := make(map[string]int)
+	for i, word := range words {
+		dict[word] = i
+	}
+
+	var out [][]int
+	for i, word := range words {
+		for j := 0; j <= len(word); j++ {
+			if isPalindrome(word[j:]) {
+				if index, ok := dict[revWord(word[:j])]; ok && index != i {
+					out = append(out, []int{i, index})
+				}
+			}
+			if j != 0 && isPalindrome(word[:j]) {
+				if index, ok := dict[revWord(word[j:])]; ok && index != i {
+					out = append(out, []int{index, i})
+				}
+			}
+		}
+	}
+	return out
+}
--- 0336_palindrome-pairs_test.go
+++ 0336_palindrome-pairs_test.go
@@ -33,3 +33,32 @@ func Test_palindromePairs(t *testing.T) {
 		})
 	}
 }
+
+func Test_palindromePairs2(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"1",
+			args{[]string{"abcd", "dcba", "lls", "s", "sssll"}},
+			[][]int{{0, 1}, {1, 0}, {3, 2}, {2, 4}},
+		},
+		{
+			"2",
+			args{[]string{"bat", "tab", "cat"}},
+			[][]int{{0, 1}, {1, 0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := palindromePairs2(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("palindromePairs2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
