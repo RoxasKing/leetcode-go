628efbc4d84c4e1616beb92d95fff4cc6e9b9229
RoxasKing <tekkenfansking@gmail.com>
2020-08-10 09:29:08 +0800

    added 0696

---
+++ 0696_count-binary-substrings_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_countBinarySubstrings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"00110011"}, 6},
+		{"2", args{"10101"}, 4},
+		{"3", args{"101001"}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countBinarySubstrings(tt.args.s); got != tt.want {
+				t.Errorf("countBinarySubstrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
