3b110a5c090a4dd40b4a48bc68bfcf30d7d24512
RoxasKing <tekkenfansking@gmail.com>
2020-08-15 09:39:40 +0800

    added 0546

---
+++ 0546_remove-boxes.go
@@ -0,0 +1,67 @@
+package leetcode
+
+/*
+  给出一些不同颜色的盒子，盒子的颜色由数字表示，即不同的数字表示不同的颜色。
+  你将经过若干轮操作去去掉盒子，直到所有的盒子都去掉为止。每一轮你可以移除具有相同颜色的连续 k 个盒子（k >= 1），这样一轮之后你将得到 k*k 个积分。
+  当你将所有盒子都去掉之后，求你能获得的最大积分和。
+
+  提示：
+    1 <= boxes.length <= 100
+    1 <= boxes[i] <= 100
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/remove-boxes
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// Dynamic Programming
+func removeBoxes(boxes []int) int {
+	dp := [100][100][100]int{}
+	var cal func(int, int, int) int
+	cal = func(l, r, k int) int {
+		if l > r {
+			return 0
+		}
+		if dp[l][r][k] != 0 {
+			return dp[l][r][k]
+		}
+		for r > l && boxes[r] == boxes[r-1] {
+			r--
+			k++
+		}
+		dp[l][r][k] = cal(l, r-1, 0) + (k+1)*(k+1)
+		for i := l; i < r; i++ {
+			if boxes[i] == boxes[r] {
+				dp[l][r][k] = Max(dp[l][r][k], cal(l, i, k+1)+cal(i+1, r-1, 0))
+			}
+		}
+		return dp[l][r][k]
+	}
+	return cal(0, len(boxes)-1, 0)
+}
+
+// backtrack(time out)
+func removeBoxes2(boxes []int) int {
+	var max int
+	var backTrack func([]int, int)
+	backTrack = func(boxes []int, cur int) {
+		if len(boxes) == 0 {
+			max = Max(max, cur)
+			return
+		}
+		var l, r int
+		for l < len(boxes) {
+			r = l
+			for r+1 < len(boxes) && boxes[r+1] == boxes[r] {
+				r++
+			}
+			newBoxes := make([]int, len(boxes)-(r+1-l))
+			copy(newBoxes[:l], boxes[:l])
+			copy(newBoxes[l:], boxes[r+1:])
+			backTrack(newBoxes, cur+(r+1-l)*(r+1-l))
+			l = r + 1
+		}
+	}
+	backTrack(boxes, 0)
+	return max
+}
