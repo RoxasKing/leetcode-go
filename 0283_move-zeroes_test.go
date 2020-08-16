0714521bd31c782084a781692941be8bc063774e
RoxasKing <tekkenfansking@gmail.com>
2020-08-16 10:39:03 +0800

    added 0283

---
+++ 0283_move-zeroes_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{0, 1, 0, 3, 12}}, []int{1, 3, 12, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+			if !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("moveZeroes() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
