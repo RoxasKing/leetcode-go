a895440be1c7277df06ac23f7982b61078f27fa2
RoxasKing <tekkenfansking@gmail.com>
2020-08-19 09:56:39 +0800

    updated 0206

---
+++ 0206_reverse-linked-list/0206_reverse-linked-list.go
@@ -0,0 +1,55 @@
+package leetcode
+
+/*
+  反转一个单链表。
+
+  进阶:
+    你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+*/
+
+// Iteration
+func reverseList(head *ListNode) *ListNode {
+	var revHead *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = revHead
+		revHead = head
+		head = next
+	}
+	return revHead
+}
+
+// Recursion
+func reverseList2(head *ListNode) *ListNode {
+	res, _ := recur(head)
+	return res
+}
+
+func recur(node *ListNode) (*ListNode, *ListNode) {
+	if node == nil || node.Next == nil {
+		return node, node
+	}
+	head, last := recur(node.Next)
+	last.Next, node.Next = node, nil
+	return head, node
+}
+
+// Recursion
+func reverseList3(head *ListNode) *ListNode {
+	var recur func(*ListNode) *ListNode
+	recur = func(node *ListNode) *ListNode {
+		if node == nil || node.Next == nil {
+			return node
+		}
+		p := recur(node.Next)
+		node.Next.Next = node
+		node.Next = nil
+		return p
+	}
+	return recur(head)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ 0206_reverse-linked-list/0206_reverse-linked-list_test.go
@@ -0,0 +1,156 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"1",
+			args{
+				&ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val:  4,
+								Next: &ListNode{Val: 5},
+							},
+						},
+					},
+				},
+			},
+			&ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  2,
+							Next: &ListNode{Val: 1},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseList2(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"1",
+			args{
+				&ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val:  4,
+								Next: &ListNode{Val: 5},
+							},
+						},
+					},
+				},
+			},
+			&ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  2,
+							Next: &ListNode{Val: 1},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList2(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseList3(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"1",
+			args{
+				&ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val:  4,
+								Next: &ListNode{Val: 5},
+							},
+						},
+					},
+				},
+			},
+			&ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  2,
+							Next: &ListNode{Val: 1},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList3(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
