035e5f1683836d642acfa7cb3573594bf2ecc7c6
RoxasKing <tekkenfansking@gmail.com>
2020-08-21 12:07:28 +0800

    updated 0198

--- 0198_house-robber/house-robber.go
+++ 0198_house-robber/house-robber.go
@@ -1,4 +1,4 @@
-package leetcode
+package main
 
 /*
   你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
@@ -10,17 +10,21 @@ package leetcode
   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
+// Dynamic Programming
 func rob(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	dp0 := nums[0]
-	dp1 := Max(nums[0], nums[1])
-	for i := 2; i < len(nums); i++ {
-		dp0, dp1 = dp1, Max(dp0+nums[i], dp1)
+	dp0, dp1 := 0, nums[0]
+	for i := 1; i < len(nums); i++ {
+		dp0, dp1 = dp1, Max(dp1, dp0+nums[i])
 	}
 	return dp1
+}
+
+func Max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
