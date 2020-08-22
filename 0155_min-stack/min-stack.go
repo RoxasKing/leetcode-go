df72e5976ed157583151f65e4567366b6b075604
RoxasKing <tekkenfansking@gmail.com>
2020-08-22 16:31:43 +0800

    updated 0155

--- 0155_min-stack/min-stack.go
+++ 0155_min-stack/min-stack.go
@@ -1,17 +1,27 @@
 package leetcode
 
-// MinStack ...
+/*
+  设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+    push(x) —— 将元素 x 推入栈中。
+    pop() —— 删除栈顶的元素。
+    top() —— 获取栈顶元素。
+    getMin() —— 检索栈中的最小元素。
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/min-stack
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 type MinStack struct {
 	stack []int
 	help  []int
 }
 
-// NewMinStack ...
-func NewMinStack() MinStack {
-	return MinStack{stack: []int{}, help: []int{}}
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{}
 }
 
-// Push ...
 func (s *MinStack) Push(x int) {
 	s.stack = append(s.stack, x)
 	if len(s.help) == 0 || x <= s.help[len(s.help)-1] {
@@ -19,7 +29,6 @@ func (s *MinStack) Push(x int) {
 	}
 }
 
-// Pop ...
 func (s *MinStack) Pop() {
 	if s.stack[len(s.stack)-1] == s.help[len(s.help)-1] {
 		s.help = s.help[:len(s.help)-1]
@@ -27,12 +36,10 @@ func (s *MinStack) Pop() {
 	s.stack = s.stack[:len(s.stack)-1]
 }
 
-// Top ...
 func (s *MinStack) Top() int {
 	return s.stack[len(s.stack)-1]
 }
 
-// GetMin ...
 func (s *MinStack) GetMin() int {
 	return s.help[len(s.help)-1]
 }
--- 0155_min-stack/min-stack_test.go
+++ 0155_min-stack/min-stack_test.go
@@ -2,11 +2,10 @@ package leetcode
 
 import (
 	"fmt"
-	"testing"
 )
 
-func Test_minStack(t *testing.T) {
-	minstack := NewMinStack()
+func ExampleMinStack() {
+	minstack := Constructor()
 	minstack.Push(-2)
 	minstack.Push(0)
 	minstack.Push(-3)
@@ -17,4 +16,10 @@ func Test_minStack(t *testing.T) {
 	minstack.Pop()
 	fmt.Println(minstack.Top())
 	fmt.Println(minstack.GetMin())
+	// Output:
+	// -3
+	// 0
+	// -2
+	// -2
+	// -2
 }
