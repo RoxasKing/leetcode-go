80be5e165de121a05cfa8cfa338345956fd97242
RoxasKing <tekkenfansking@gmail.com>
2020-08-23 09:10:37 +0800

    added 0201

---
+++ 0201_bitwise-and-of-numbers-range/bitwise-and-of-numbers-range_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_rangeBitwiseAnd(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{5, 7}, 4},
+		{"2", args{0, 1}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rangeBitwiseAnd(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("rangeBitwiseAnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_rangeBitwiseAnd2(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{5, 7}, 4},
+		{"2", args{0, 1}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rangeBitwiseAnd2(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("rangeBitwiseAnd2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
