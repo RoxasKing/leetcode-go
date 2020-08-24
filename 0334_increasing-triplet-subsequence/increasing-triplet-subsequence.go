2f0fe93a4712649d68800a3ba31386d966adfb68
RoxasKing <tekkenfansking@gmail.com>
2020-08-24 13:18:25 +0800

    updated 0334

--- 0334_increasing-triplet-subsequence/increasing-triplet-subsequence.go
+++ 0334_increasing-triplet-subsequence/increasing-triplet-subsequence.go
@@ -31,3 +31,30 @@ func increasingTriplet(nums []int) bool {
 	}
 	return false
 }
+
+// Monotone Stack
+func increasingTriplet2(nums []int) bool {
+	if len(nums) < 3 {
+		return false
+	}
+	var stack []int
+	for i := 0; i < len(nums); i++ {
+		for len(stack) > 0 && nums[i] <= stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, nums[i])
+		if len(stack) == 3 {
+			return true
+		}
+	}
+	for i := len(nums) - 1; i >= 0; i-- {
+		for len(stack) > 0 && nums[i] >= stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, nums[i])
+		if len(stack) == 3 {
+			return true
+		}
+	}
+	return false
+}
