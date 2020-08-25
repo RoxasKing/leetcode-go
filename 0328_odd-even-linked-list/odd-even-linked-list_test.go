446666cb99040029299479cbcc8b382038d03fb7
RoxasKing <tekkenfansking@gmail.com>
2020-08-25 15:24:42 +0800

    added 0328

---
+++ 0328_odd-even-linked-list/odd-even-linked-list_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_oddEvenList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"1",
+			args{
+				&ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val:  4,
+								Next: &ListNode{Val: 5},
+							},
+						},
+					},
+				},
+			},
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val:  2,
+							Next: &ListNode{Val: 4},
+						},
+					},
+				},
+			},
+		},
+		{
+			"2",
+			args{
+				&ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val:  3,
+							Next: &ListNode{Val: 4},
+						},
+					},
+				},
+			},
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val:  2,
+						Next: &ListNode{Val: 4},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := oddEvenList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("oddEvenList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
