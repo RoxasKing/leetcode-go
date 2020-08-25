cb3e32d8570939e573152fc4db5c80b76577548a
RoxasKing <tekkenfansking@gmail.com>
2020-08-25 14:45:06 +0800

    updated 0002

--- 0002_add-two-numbers/add-two-numbers.go
+++ 0002_add-two-numbers/add-two-numbers.go
@@ -11,35 +11,24 @@ package main
 */
 
 // Elementary mathematics
-func addTwoNumbers(l1, l2 *ListNode) *ListNode {
-	head := new(ListNode)
-	tail := head
-	var val, rest int
-	for {
-		val = rest
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	pre := &ListNode{}
+	cur := pre
+	var tmp int
+	for l1 != nil || l2 != nil || tmp != 0 {
 		if l1 != nil {
-			val += l1.Val
+			tmp += l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
-			val += l2.Val
+			tmp += l2.Val
 			l2 = l2.Next
 		}
-		if val == 0 && l1 == nil && l2 == nil {
-			if head.Next == nil {
-				return head
-			}
-			return head.Next
-		}
-		if val >= 10 {
-			val -= 10
-			rest = 1
-		} else {
-			rest = 0
-		}
-		tail.Next = &ListNode{Val: val, Next: nil}
-		tail = tail.Next
+		cur.Next = &ListNode{Val: tmp % 10}
+		cur = cur.Next
+		tmp /= 10
 	}
+	return pre.Next
 }
 
 type ListNode struct {
