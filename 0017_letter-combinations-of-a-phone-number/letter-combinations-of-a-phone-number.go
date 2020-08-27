a4596ee6d0244dbd7eaf1c5c0eea15b1f399cb11
RoxasKing <tekkenfansking@gmail.com>
2020-08-27 16:33:53 +0800

    updated 0017

--- 0017_letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
+++ 0017_letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
@@ -6,6 +6,7 @@ package main
   https://assets.leetcode-cn.com/aliyun-lc-upload/original_images/17_telephone_keypad.png
 */
 
+// Iteration
 func letterCombinations(digits string) []string {
 	if digits == "" {
 		return nil
@@ -31,5 +32,36 @@ func letterCombinations(digits string) []string {
 		}
 		out = tmp
 	}
+	return out
+}
+
+// Backtracking
+func letterCombinations2(digits string) []string {
+	if digits == "" {
+		return nil
+	}
+	dict := map[int][]string{
+		2: {"a", "b", "c"},
+		3: {"d", "e", "f"},
+		4: {"g", "h", "i"},
+		5: {"j", "k", "l"},
+		6: {"m", "n", "o"},
+		7: {"p", "q", "r", "s"},
+		8: {"t", "u", "v"},
+		9: {"w", "x", "y", "z"},
+	}
+	var out []string
+	var backtrack func(int, string)
+	backtrack = func(index int, str string) {
+		if len(str) == len(digits) {
+			out = append(out, str)
+			return
+		}
+		letters := dict[int(digits[index]-'0')]
+		for _, letter := range letters {
+			backtrack(index+1, str+letter)
+		}
+	}
+	backtrack(0, "")
 	return out
 }
--- 0017_letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number_test.go
+++ 0017_letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number_test.go
@@ -28,3 +28,27 @@ func Test_letterCombinations(t *testing.T) {
 		})
 	}
 }
+
+func Test_letterCombinations2(t *testing.T) {
+	type args struct {
+		digits string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			"1",
+			args{"23"},
+			[]string{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := letterCombinations2(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("letterCombinations2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
