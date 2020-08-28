cbd28fdd963beac909f84849d57c6c2fa6022590
RoxasKing <tekkenfansking@gmail.com>
2020-08-28 13:22:41 +0800

    updated 0162

--- 0162_find-peak-element/find-peak-element_test.go
+++ 0162_find-peak-element/find-peak-element_test.go
@@ -9,14 +9,19 @@ func Test_findPeakElement(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
-		{"1", args{[]int{1, 2, 3, 1}}, 2},
-		{"2", args{[]int{1, 2, 1, 3, 5, 6, 4}}, 5},
+		{"1", args{[]int{1, 2, 3, 1}}, []int{2}},
+		{"2", args{[]int{1, 2, 1, 3, 5, 6, 4}}, []int{1, 5}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findPeakElement(tt.args.nums); got != tt.want {
+			got := findPeakElement(tt.args.nums)
+			mark := make(map[int]bool)
+			for _, num := range tt.want {
+				mark[num] = true
+			}
+			if !mark[got] {
 				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
 			}
 		})
