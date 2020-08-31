7e9184f1ada224a5a27db99ad05c591409a10d4e
RoxasKing <tekkenfansking@gmail.com>
2020-08-31 19:38:51 +0800

    added 0841

---
+++ 0841_keys-and-rooms/keys-and-rooms_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_canVisitAllRooms(t *testing.T) {
+	type args struct {
+		rooms [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{[][]int{{1}, {2}, {3}, {1}}}, true},
+		{"2", args{[][]int{{1, 3}, {3, 0, 1}, {2}, {0}}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canVisitAllRooms(tt.args.rooms); got != tt.want {
+				t.Errorf("canVisitAllRooms() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
