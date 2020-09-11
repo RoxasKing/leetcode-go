98fa8b64345ea95c1eb04ba87b2855cafc3e97c9
RoxasKing <tekkenfansking@gmail.com>
2020-09-11 18:30:52 +0800

    updated 0076

--- 0076_minimum-window-substring/minimum-window-substring.go
+++ 0076_minimum-window-substring/minimum-window-substring.go
@@ -6,25 +6,24 @@ package main
 
 // Sliding Window
 func minWindow(s string, t string) string {
-	have, need := [128]int{}, [128]int{}
-	for _, c := range t {
-		need[c]++
-	}
 	out := s
-	var l, r, count int
-	for r < len(s) {
-		if have[s[r]] < need[s[r]] {
+	var count int
+	have := [128]int{}
+	for i := range t {
+		have[t[i]]++
+	}
+	for l, r := 0, 0; r < len(s); r++ {
+		if have[s[r]] > 0 {
 			count++
 		}
-		have[s[r]]++
-		for l < r && have[s[l]] > need[s[l]] {
-			have[s[l]]--
+		have[s[r]]--
+		for l < r && have[s[l]] < 0 {
+			have[s[l]]++
 			l++
 		}
 		if count == len(t) && r+1-l < len(out) {
 			out = s[l : r+1]
 		}
-		r++
 	}
 	if count < len(t) {
 		return ""
