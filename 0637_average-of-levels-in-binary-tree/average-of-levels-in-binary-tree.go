3827c38721861abea88d32cbdd397effbc66c4c4
RoxasKing <tekkenfansking@gmail.com>
2020-09-12 09:26:27 +0800

    added 0637

---
+++ 0637_average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+  给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+
+  提示：
+    节点值的范围在32位有符号整数范围内。
+*/
+
+// BFS
+func averageOfLevels(root *TreeNode) []float64 {
+	var out []float64
+	q := []*TreeNode{root}
+	for len(q) != 0 {
+		curSize := len(q)
+		var sum float64
+		for _, n := range q {
+			sum += float64(n.Val)
+			if n.Left != nil {
+				q = append(q, n.Left)
+			}
+			if n.Right != nil {
+				q = append(q, n.Right)
+			}
+		}
+		out = append(out, sum/float64(curSize))
+		q = q[curSize:]
+	}
+	return out
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
