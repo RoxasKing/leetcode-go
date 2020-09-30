b3f7e67f526537a3e85ac87033c2ae3c6149998e
RoxasKing <tekkenfansking@gmail.com>
2020-09-30 09:36:31 +0800

    added 0701

---
+++ 0701_insert-into-a-binary-search-tree/insert-into-a-binary-search-tree_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_insertIntoBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"1",
+			args{
+				&TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 1},
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{Val: 7},
+				},
+				5,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := insertIntoBST(tt.args.root, tt.args.val)
+			node := got
+			preVal := -1 << 31
+			for node != nil {
+				if node.Left != nil {
+					pre := node.Left
+					for pre.Right != nil && pre.Right != node {
+						pre = pre.Right
+					}
+					if pre.Right != node {
+						pre.Right = node
+						node = node.Left
+						continue
+					}
+					pre.Right = nil
+				}
+				if node.Val < preVal {
+					t.Error("insertIntoBST() test fail", got)
+				}
+				preVal = node.Val
+				node = node.Right
+			}
+		})
+	}
+}
